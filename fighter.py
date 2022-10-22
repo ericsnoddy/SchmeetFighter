a7bbf7b05efa8c8693979d8dd118670515900293
ericsnoddy <ericsnoddy@gmail.com>
2022-10-22 18:04:47 -0400

    ui: score, countdown, victory, reset, sound fx

--- fighter.py
+++ fighter.py
@@ -3,23 +3,31 @@
 import pygame as pg
 
 class Fighter:
-    def __init__(self, x, y, player_data, sprite_sheet, frame_number_list):
+    def __init__(self, player_x, x, y, flip, player_data, sprite_sheet, frame_number_list, sound_fx):
 
+        self.player_x = player_x
+        self.sound_fx = sound_fx
         self.size = player_data[0]  # px^2
         self.scale = player_data[1]  # image scaling
         self.offset = player_data[2]  # px offset        
         self.anim_list = self.load_images(sprite_sheet, frame_number_list)
         self.action = 0  # 0: idle, 1: run, 2: jump, 3: attack1, 4: attack2, 5: hit, 6: death
         self.frame_index = 0
+        self.update_time = pg.time.get_ticks()
         self.image = self.anim_list[self.action][self.frame_index]
         self.rect = pg.Rect(x, y, 80, 180)
 
         self.vel_y = 0
-        self.flip = False
+        self.flip = flip
+        self.running = False
         self.jumping = False
         self.attacking = False
         self.attack_type = 0
+        self.attack_cooldown = 0
+        self.hit = False
+        self.damage = 10
         self.health = 100
+        self.alive = True
 
     def load_images(self, sprite_sheet, frame_number_list):
 
@@ -39,36 +47,71 @@ class Fighter:
         
         return anim_list
 
-    def move(self, surf, screen_width, screen_height, target):
+    def move(self, surf, screen_width, screen_height, target, round_over):
 
         SPEED = 10
         GRAVITY = 2
+
+        # reset variables
         move_vect = pg.math.Vector2(0,0)
+        self.running = False
+        self.attack_type = 0
 
         # get key presses
         key = pg.key.get_pressed()
 
         # perform movements only if not attacking
-        if not self.attacking:
-            # movement
-            if key[pg.K_a]:
-                move_vect[0] = -SPEED
-            if key[pg.K_d]:
-                move_vect[0] = SPEED
+        if not self.attacking and self.alive and not round_over:
 
-            # jumping
-            if key[pg.K_w] and not self.jumping:
-                self.vel_y = -30
-                self.jumping = True
+            # check player 1 controls
+            if self.player_x == 1:
+                # movement
+                if key[pg.K_a]:
+                    move_vect[0] = -SPEED
+                    self.running = True
+                if key[pg.K_d]:
+                    move_vect[0] = SPEED
+                    self.running = True
 
-            # attacking
-            if key[pg.K_r] or key[pg.K_t]:
-                self.attack(surf, target)
-                # determine attack type
-                if key[pg.K_r]:
-                    self.attack_type == 1
-                if key[pg.K_t]:
-                    self.attack_type == 2
+                # jumping
+                if key[pg.K_w] and not self.jumping:
+                    self.vel_y = -30
+                    self.jumping = True
+
+                # attacking
+                if key[pg.K_r] or key[pg.K_t]:
+                    self.attack(surf, target)
+                    # determine attack type
+                    if key[pg.K_r]:
+                        self.attack_type = 1
+                        self.damage = 10
+                    if key[pg.K_t]:
+                        self.attack_type = 2
+                        self.damage = 15
+
+            # check player 2 controls
+            if self.player_x == 2:
+                # movement
+                if key[pg.K_LEFT]:
+                    move_vect[0] = -SPEED
+                    self.running = True
+                if key[pg.K_RIGHT]:
+                    move_vect[0] = SPEED
+                    self.running = True
+
+                # jumping
+                if key[pg.K_UP] and not self.jumping:
+                    self.vel_y = -30
+                    self.jumping = True
+
+                # attacking
+                if key[pg.K_PAGEUP] or key[pg.K_PAGEDOWN]:
+                    self.attack(surf, target)
+                    # determine attack type
+                    if key[pg.K_PAGEUP]:
+                        self.attack_type = 1
+                    if key[pg.K_PAGEDOWN]:
+                        self.attack_type = 2
 
         # apply gravity
         self.vel_y += GRAVITY
@@ -89,22 +132,90 @@ class Fighter:
             self.flip = False
         else: self.flip = True
 
+        # apply attack cooldown
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
         # update position
         self.rect.x += move_vect[0]
         self.rect.y += move_vect[1]
 
+    def update(self):
+
+        # check action to determine animation
+        if self.health <= 0:
+            self.health = 0
+            self.alive = False
+            self.update_action(6)  # death
+        elif self.hit:
+            self.update_action(5)  # hit
+        elif self.attacking:
+            if self.attack_type == 1:
+                self.update_action(3)  # attack 1
+            elif self.attack_type == 2:
+                self.update_action(4)  # attack 2
+        elif self.jumping:
+            self.update_action(2)  # jump
+        elif self.running:
+            self.update_action(1)  # run
+        else: 
+            self.update_action(0)  # idle
+
+        COOLDOWN = 75
+        now = pg.time.get_ticks()
+
+        if now - self.update_time > COOLDOWN:
+            self.image = self.anim_list[self.action][self.frame_index]
+            self.frame_index += 1
+            self.update_time = now
+
+        # ensure index is within range (animation has finished)
+        if self.frame_index >= len(self.anim_list[self.action]):
+
+            # halt animation if dead
+            if not self.alive:
+                self.frame_index = len(self.anim_list[self.action]) - 1
+            else:
+                self.frame_index = 0
+                # check if attack executed
+                if self.action == 3:
+                    self.attacking = False
+                    self.attack_cooldown = 20
+                if self.action == 4:
+                    self.attacking = False
+                    self.attack_cooldown = 30
+                # check if damage was taken
+                if self.action == 5:
+                    self.hit = False
+                    # if player in middle of attack, halt attack; successful parry
+                    self.attacking = False
+                    self.attack_cooldown = 25
+
     def attack(self, surf, target):
 
-        self.attacking = True
+        # only if attack is allowed
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            self.sound_fx.play()            
 
-        # create rect for attack 2x as wide as character, accounting for facing direction
-        attacking_rect = pg.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
-        if attacking_rect.colliderect(target.rect):
-            target.health -= 10
+            # create rect for attack 2x as wide as character, accounting for facing direction
+            attacking_rect = pg.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+            if attacking_rect.colliderect(target.rect):
+                target.health -= self.damage
+                target.hit = True
 
-        pg.draw.rect(surf, (0, 255, 0), attacking_rect)
+
+    def update_action(self, new_action):
+
+        # check if new action is different than previous
+        if new_action != self.action:
+            self.action = new_action
+
+            # update animation settings
+            self.frame_index = 0
+            self.update_time = pg.time.get_ticks()
 
     def draw(self, surf):
 
-        pg.draw.rect(surf, (255, 0, 0), self.rect)
-        surf.blit(self.image, (self.rect.x - (self.offset[0] * self.scale), self.rect.y - (self.offset[1] * self.scale)))
+        img = pg.transform.flip(self.image, self.flip, False)
+        surf.blit(img, (self.rect.x - (self.offset[0] * self.scale), self.rect.y - (self.offset[1] * self.scale)))
--- main.py
+++ main.py
@@ -14,13 +14,13 @@ WIDTH = 1000
 HEIGHT = 600
 
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption('Schmeet Fighter 2')
+pg.display.set_caption('Schmeet Fighter')
 
 # set framerate
 clock = pg.time.Clock()
 FPS = 60
 
-# colors
+# define color constants
 YELLOW = (255, 255, 0)
 RED = (255, 0 , 0)
 WHITE = (255, 255, 255)
@@ -29,10 +29,20 @@ WHITE = (255, 255, 255)
 bg_img = pg.image.load(join('assets', 'images', 'background', 'background.jpg')).convert_alpha()
 
 # load sprite sheets
-warrior_sheet = pg.image.load(join('assets', 'images', 'warrior', 'Sprites', 'warrior.png'))
-wizard_sheet = pg.image.load(join('assets', 'images', 'wizard', 'Sprites', 'wizard.png'))
+warrior_sheet = pg.image.load(join('assets', 'images', 'warrior', 'Sprites', 'warrior.png')).convert_alpha()
+wizard_sheet = pg.image.load(join('assets', 'images', 'wizard', 'Sprites', 'wizard.png')).convert_alpha()
 
-# define fighter variables
+# load victory image
+victory_img = pg.image.load(join('assets', 'images', 'icons', 'victory.png')).convert_alpha()
+
+# define game variables
+intro_count = 3
+last_count_update = round_over_time = pg.time.get_ticks()
+round_over = False
+ROUND_OVER_COOLDOWN = 2500
+score = [0, 0]  # [player 1 score, player 2 score]
+
+# define fighter constants
 WARRIOR_SIZE = 162  # px^2
 WARRIOR_SCALE = 4
 WARRIOR_OFFSET = [72, 56]
@@ -46,15 +56,36 @@ WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 WARRIOR_ANIM_FRAMES = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIM_FRAMES = [8, 8, 1, 8, 8, 3, 7]
 
+# load sounds
+pgm = pg.mixer.music
+pgs = pg.mixer.Sound
+pgm.load(join('assets', 'audio', 'music.mp3'))
+pgm.set_volume(0.5)
+pgm.play(-1, 0.0, 5000)
+sword_fx = pgs(join('assets', 'audio', 'sword.wav'))
+sword_fx.set_volume(0.5)
+staff_fx = pgs(join('assets', 'audio', 'magic.wav'))
+staff_fx.set_volume(0.75)
+
 # create two fighters
-fighter_1 = Fighter(200, 310, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIM_FRAMES)
-fighter_2 = Fighter(700, 310, WIZARD_DATA, wizard_sheet, WIZARD_ANIM_FRAMES)
+fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIM_FRAMES, sword_fx)
+fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIM_FRAMES, staff_fx)
+
+# load fonts
+count_font = pg.font.Font(join('assets', 'fonts', 'turok.ttf'), 150)
+victory_font = pg.font.Font(join('assets', 'fonts', 'turok.ttf'), 130)
+score_font = pg.font.Font(join('assets', 'fonts', 'turok.ttf'), 30)
 
 # FUNCTIONS
 def draw_bg():
 
     scaled_bg = pg.transform.scale(bg_img, (WIDTH, HEIGHT))
     WIN.blit(scaled_bg, (0,0))
+
+def draw_text(text, font, color, x, y):
+
+    img = font.render(text, True, color)
+    WIN.blit(img, (x, y))
 
 def draw_health_bar(health, x, y):
 
@@ -81,13 +112,57 @@ while running:
     draw_health_bar(fighter_1.health, 20, 20)
     draw_health_bar(fighter_2.health, 580, 20)
 
-    # move fighters
-    fighter_1.move(WIN, WIDTH, HEIGHT, fighter_2)
-    # fighter_2.move()
+    # display scores
+    draw_text('Player 1 KOs: ' + str(score[0]), score_font, RED, 20, 60)
+    draw_text('Player 2 KOs: ' + str(score[1]), score_font, RED, 580, 60)
+
+    # move fighters after intro countdown
+    if intro_count <= 0:
+        fighter_1.move(WIN, WIDTH, HEIGHT, fighter_2, round_over)
+        fighter_2.move(WIN, WIDTH, HEIGHT, fighter_1, round_over)
+    else:
+        # display countdown
+        draw_text(str(intro_count), count_font, RED, (WIDTH - 58) / 2, (HEIGHT - 168) / 4)
+        # update countdown
+        now = pg.time.get_ticks()
+        if now - last_count_update >= 1000:
+            intro_count -= 1
+            last_count_update = now
+            
+    # update fighters
+    fighter_1.update()
+    fighter_2.update()
 
     # draw fighters
     fighter_1.draw(WIN)
     fighter_2.draw(WIN)
+
+    # check for defeat
+    if not round_over:
+        now = pg.time.get_ticks()
+        if not fighter_1.alive:
+            score[1] += 1
+            round_over = True
+            round_over_time = now
+        if not fighter_2.alive:
+            score[0] += 1
+            round_over = True
+            round_over_time = now
+    else:
+        # display victory image
+        if (fighter_1.alive and not fighter_2.alive) or (fighter_2.alive and not fighter_1.alive):
+            draw_text('VICTORY!', victory_font, RED, (WIDTH - 462) / 2, (HEIGHT - 146) / 4)
+        elif not fighter_1.alive and not fighter_2.alive:
+            draw_text('DOUBLE KO!', victory_font, RED, (WIDTH - 585) / 2, (HEIGHT - 146) / 4)
+
+        now = pg.time.get_ticks()
+        if now - round_over_time > ROUND_OVER_COOLDOWN:
+            round_over = False
+            intro_count = 3
+            last_count_update = now            
+            fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIM_FRAMES, sword_fx)
+            fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIM_FRAMES, staff_fx)
+
 
     # event handler
     for event in pg.event.get():
